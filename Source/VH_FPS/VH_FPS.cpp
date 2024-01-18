92b5d7b510a930ca8df8004d3150fa80809eb0e4
Viiktorh <37716261+Viiktorh@users.noreply.github.com>
2024-01-18 14:17:07 +0100

    Relocated

---
+++ Source/VH_FPS/VH_FPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VH_FPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VH_FPS, "VH_FPS" );
